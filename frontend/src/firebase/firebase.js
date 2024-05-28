d344ed6d810cf4d8db59b1e00ebdcdf03d5ace53
IIRobotNinjaII <antonythoppil@hotmail.com>
2024-05-28 10:15:37 +0400

    Update firebase.js

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2ajDNYFdICcjADX5eY2pZ_VmHgrDXMgM",
+  apiKey: "",
   authDomain: "tellicherry-cricket-academy.firebaseapp.com",
   projectId: "tellicherry-cricket-academy",
   storageBucket: "tellicherry-cricket-academy.appspot.com",
-  messagingSenderId: "686572371447",
-  appId: "1:686572371447:web:2a8d5bba5398dbf7f042f2",
-  measurementId: "G-PZP9J7Q6S9"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
